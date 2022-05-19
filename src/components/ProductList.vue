eac0e70d9a30ce428e209274c6144015d72183d8
Gilson.Conceicao <gilson.conceicao@al.infnet.edu.br>
2022-05-19 05:46:44 -0300

    product list

---
+++ src/components/ProductList.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <h1>Nicollas Petit And Alexs Dubois Products</h1>
+<ProductListItem 
+v-for="(item, index) in items"
+:key="index"
+:item="item"
+/>
+    
+  </div>
+</template>
+
+<script>
+import ProductListItem from './ProductListItem.vue'
+
+export default {
+  props: {
+    items: {
+      type: Array,
+      default: () => [],
+    },
+  },
+  components: {
+    ProductListItem,
+  },
+
+};
+</script>
+
+<style></style>
