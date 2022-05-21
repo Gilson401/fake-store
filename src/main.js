6cae4e6a8bcf69aeefc876db4b18f7036c21566a
Gilson.Conceicao <gilson.conceicao@al.infnet.edu.br>
2022-05-21 19:35:37 -0300

    css global

--- src/main.js
+++ src/main.js
@@ -3,7 +3,8 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
-import './assets/main.css'
+import './assets/reset.css'
+import './assets/global.css'
 
 Vue.config.productionTip = false
 
