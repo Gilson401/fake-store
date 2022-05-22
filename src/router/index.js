39e4998731ed853fac1e2f72ad78e4f696ce0222
Gilson.Conceicao <gilson.conceicao@al.infnet.edu.br>
2022-05-22 17:17:34 -0300

    remoção rotas desnecessárias

--- src/router/index.js
+++ src/router/index.js
@@ -1,33 +1,20 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
 import Products from '../views/Products.vue'
 import Cart from '../views/Cart.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
+
   {
     path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  },
-  {
-    path: '/products',
-    name: '',
+    name: 'Produtos',
     component: Products
   },
   {
     path: '/cart',
-    name: '',
+    name: 'Carrinho',
     component: Cart
   }
 ]
