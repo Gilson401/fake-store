0664a23de02e9b4d11de2fc471333b78c74a44af
Gilson.Conceicao <gilson.conceicao@al.infnet.edu.br>
2022-05-22 17:07:57 -0300

    finalizaão do menu

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,9 @@
 <template>
   <div id="app" class="container flex flex-col">
-    <header class="flex-rc space-between menu center">  
+    <header class="flex-rc space-between menu center pad-xy-10">  
       <h1 class="title ">Nicollas Petit And Alexs Dubois Products</h1>
       <div id="nav" class="pad-y-10">
-        <router-link to="/">Home</router-link> |
-        <router-link to="/about">Sobre</router-link> |
-        <router-link to="/products">Produtos</router-link> |
+        <router-link to="/">Produtos</router-link> |
         <router-link to="/cart">Carrinho</router-link>
       </div>
     </header>
@@ -22,13 +20,42 @@
   </div>
 </template>
 
+<script>
+
+export default {
+   async mounted() {
+    await this.listProducts();  
+  },
+  computed: {
+    products() {
+      return this.$store.getters.products;
+    },
+      
+
+  },
+  methods: {
+    async listProducts() {
+    this.isLoading = true;
+      this.error = ''
+      try {
+        await this.$store.dispatch("index");
+      } catch (error) {
+        this.error = "Erro na listagem de produto"
+      }
+      this.isLoading = false;
+    },
+  },
+}
+</script>
+
+
 <style>
 .menu {
-    padding: 20px; 
-    position: sticky;
+     position: sticky;
     z-index: 10;
     top: 0;
     background-color: white;
+    gap: 1rem;
 }
 .content{
     flex-grow: 1;
@@ -36,11 +63,11 @@
 
 .footer {
     margin-top: auto;
+    background-color: rgba(0, 0, 0, 0.8);
+    color: #FFF;
 }
 
 #app {
-  
-
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
@@ -52,15 +79,20 @@
 }
 
 #nav a {
-  color: #2c3e50;
-  
+  color: #2c3e50;  
+  text-decoration: none;
 }
 
 #nav a.router-link-exact-active {
-  color: #42b983;
+  border-bottom: 2px rgba(50, 12, 200, 0.5) solid;
 }
 
-@media screen and (min-width: var(--breakpoint-md)) {
+@media screen and (min-width: 768px) {
+    .menu{
+        flex-direction: row;
+    }
+}   
+@media screen and (min-width: 468px) {
 
 #nav {
     margin: unset;
