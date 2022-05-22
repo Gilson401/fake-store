4b6344f3d44eb6c22cb8a08f27056bfa208a43f8
Gilson.Conceicao <gilson.conceicao@al.infnet.edu.br>
2022-05-22 17:17:53 -0300

    remoção de métodos não usados

--- src/services/products.js
+++ src/services/products.js
@@ -1,20 +1,7 @@
 import { http } from './config'
 
 export default {
-
-    create: (product) => {
-        return http.post('products', product);
-    },
-
-    update: (product) => {
-        return http.put('products', product);
-    },
-
     read: () => {
         return http.get('products')
     },
-
-    delete: (product) => {
-        return http.delete('products', { data: product })
-    }
 }
