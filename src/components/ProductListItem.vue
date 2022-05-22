c3b032f6d97dfeea8293342b98c857a84fe69ec8
Gilson.Conceicao <gilson.conceicao@al.infnet.edu.br>
2022-05-22 07:16:13 -0300

    skeleton image ratings

--- src/components/ProductListItem.vue
+++ src/components/ProductListItem.vue
@@ -1,38 +1,43 @@
 <template>
   <div class="product">
-    <img :src="item.image" alt="image" class="product__image" />
+    <img
+      v-show="hasLoadedImage"
+      :src="item.image"
+      alt="image"
+      class="product__image"
+      loading="eager"
+      @load="onLoad"
+    />
+    <div v-show="!hasLoadedImage" class="product__image loading" />
+
     <div class="product__information">
       <div class="product__title">{{ item.title }}</div>
+      <div class="product__rating">
+          <span v-for="rate in rating" :key="rate"> &#x2605;</span>
+          <span> ({{item.rating.count}})</span>
+        </div>
       <div class="product__description">{{ item.description }}</div>
-      <div class="product__price">{{ item.price }}</div>
-      <div class="product__ref">Ref.: {{ item.id }}</div>
-      
-        <div class="row">
+      <div class="flex space-between">
+        <div class="product__price">R$ {{ item.price }}</div>
+        
+        
 
-    <button class="cart-button"
-    @click="addToCart()"
-    >
-      <span class="add-to-cart"> +  </span>
-      <span class="added"></span>
-      <i class="fa fa-shopping-cart"></i>
-    </button>
-<span>{{itemQtdOnCart}}</span>
-    <button class="cart-button"
-    @click="removeFromCart()"
-    >
-      <span class="add-to-cart"> -  </span>
-      <span class="added"></span>
-      <i class="fa fa-shopping-cart"></i>
-    </button>
-
-  </div>
-
+        <div class="product__ref">Ref.: {{ item.id }}</div>
+      </div>
+      <div class="row">
+        <button class="cart-button" @click="addToCart()">
+          <span class="add-to-cart"> + </span>
+        </button>
+        <span>{{ itemQtdOnCart }}</span>
+        <button class="cart-button" @click="removeFromCart()">
+          <span class="add-to-cart"> - </span>
+        </button>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
-
 export default {
   props: {
     item: {
@@ -40,28 +45,45 @@ export default {
       default: () => {},
     },
   },
-  computed:{
-  itemQtdOnCart() {
-      return this.$store.getters.itemsOnCartIdCount(this.item.id)
+  data() {
+    return {
+      hasLoadedImage: false,
+    };
+  },
+  watch: {
+    filterBy() {
+      this.hasLoadedImage = false;
     },
   },
-  methods:{
-      addToCart(){
-          this.$store.commit('ADD_ITEM_CART', this.item)          
-      },
-      removeFromCart(){
-          this.$store.commit('REMOVE_ITEM_CART', this.item.id)          
-      },
-    
-  }
-
+  computed: {
+    itemQtdOnCart() {
+      return this.$store.getters.itemsOnCartIdCount(this.item.id);
+    },
+    filterBy() {
+      return this.$store.getters.filterBy;
+    },
+    rating() {
+      const  numerador = this.item.rating.rate - this.$store.getters.rateRange.min;
+      const denominador  =this.$store.getters.rateRange.max - this.$store.getters.rateRange.min;
+      const range = (numerador / denominador) * 5;
+      return parseInt(range) || 0;
+    },
+  },
+  methods: {
+    addToCart() {
+      this.$store.commit("ADD_ITEM_CART", this.item);
+    },
+    removeFromCart() {
+      this.$store.commit("REMOVE_ITEM_CART", this.item.id);
+    },
+    onLoad() {
+      this.hasLoadedImage = true;
+    },
+  },
 };
 </script>
 
 <style>
-
-@import url("https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;700&display=swap");
-
 .row {
   margin: 0;
   padding: 0;
@@ -160,39 +182,15 @@ export default {
   }
 }
 
-.pqt-plus,
-.pqt-minus {
-  background: #fff;
-  border: none;
-  font-size: 20px;
-  padding: 10px 20px;
-  width: 50px;
-  border-radius: 10px;
-  height: 50px;
-  user-select: none;
-  line-height: 50px;
-}
-
-.pqt-plus:hover,
-.pqt-minus:hover {
-  background: #202431;
-  color: #fff;
-  cursor: pointer;
-}
-
-
 /* ################################## */
 
-
-
 .product {
-  width: 90vw;
   display: flex;
   flex-direction: column;
   gap: 1rem;
   padding: 0.5rem;
   margin: 0.5rem auto;
-  border: solid gray 2px;
+  border: solid rgb(230, 227, 227) 1px;
   border-radius: 0.25rem;
 }
 
@@ -200,22 +198,32 @@ export default {
   width: 300px;
   height: 200px;
   object-fit: scale-down;
-  border: solid gray 2px;
+  border: solid rgb(230, 227, 227) 1px;
   flex-shrink: 0;
   flex-grow: 0;
   margin: 0.5rem auto;
+  border-radius: 5px;
+  overflow: hidden;
 }
 
 .product__information {
   text-align: left;
+  margin: 8px;
+  padding: 8px;
+  background-color: rgba(0,0,0,.8);
+  border-radius: 5px;
+  overflow: hidden;
+  color: rgb(231, 231, 231);
 }
 
 .product__title {
-  font-size: 2rem;
+  font-size: 1.5rem;
+  margin-bottom: var(--margin-1);
 }
 
 .product__description {
-  font-size: 1.25rem;
+  font-size: var(--font-4);
+  margin-bottom: var(--margin-1);
 }
 
 .product__price {
@@ -224,7 +232,13 @@ export default {
 
 .product__ref {
   font-size: 1rem;
-  font-weight: bold;
+
+}
+
+.product__rating{
+    color: rgb(255, 241, 112);
+    padding: 0.3rem;
+    border-radius: 5px;
 }
 
 @media screen and (min-width: 468px) {
@@ -233,7 +247,21 @@ export default {
   }
 
   .product__image {
-  margin: 0.5rem;
+    margin: 0.5rem;
+  }
+}
+
+.loading {
+  cursor: progress;
+  background: linear-gradient(0.25turn, transparent, #fff, transparent),
+    linear-gradient(#eee, #eee), linear-gradient(#eee, #eee);
+  background-repeat: cover;
+  animation: loading 1.5s infinite;
+}
+
+@keyframes loading {
+  to {
+    background-position: 315px 0, 0 0, 0 190px, 50px 195px;
   }
 }
 </style>
