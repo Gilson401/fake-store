5346c6b23d58ae6aca954039441598a34a87c5a9
Gilson.Conceicao <gilson.conceicao@al.infnet.edu.br>
2022-05-23 06:50:33 -0300

    page 404 default

---
+++ src/views/NotFound.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="not-found flex center flex-col">
+      <h1>Não encontrou o que procura? </h1>
+      <p>Parece que você digitou o endereço errado. Volte para home:</p>
+      <router-link to="/">Produtos</router-link>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+.not-found{
+    height: 80vh;
+    gap: 2rem;
+}
+</style>
