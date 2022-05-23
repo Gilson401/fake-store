b54989e8215fdb391b90783632f08799cc5e3ad8
Gilson.Conceicao <gilson.conceicao@al.infnet.edu.br>
2022-05-22 23:10:32 -0300

    botão to top

---
+++ src/components/TopButton.vue
@@ -0,0 +1,122 @@
+<template>
+  <div>
+    <button class="btnfixed" v-show="exibe" @click="scrollToTop">
+      &uArr;
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TopButton",
+  data() {
+    return {
+      exibe: false,
+    };
+  },
+  mounted() {
+    window.addEventListener("scroll", this.scrollFunction, true);
+  },
+  beforeDestroy(){
+    window.removeEventListener("scroll", this.scrollFunction, true);
+  },
+  methods: {
+    scrollFunction() {
+      const limite = 500;
+
+      if (
+        document.body.scrollTop > limite ||
+        document.documentElement.scrollTop > limite
+      ) {
+        this.exibe = true;
+      } else {
+        this.exibe = false;
+      }
+    },
+
+    scrollToTop() {
+      const c = document.documentElement.scrollTop || document.body.scrollTop;
+      if (c > 0) {
+        window.requestAnimationFrame(this.scrollToTop);
+        window.scrollTo(0, c - c / 8);
+      }
+    },
+  },
+};
+</script>
+
+<style>
+.animated {
+  -webkit-animation-duration: 2s;
+  animation-duration: 2s;
+  -webkit-animation-fill-mode: both;
+  animation-fill-mode: both;
+  -webkit-animation-name: fadeIn;
+  animation-name: fadeIn;
+  border: none !important;
+}
+
+@-webkit-keyframes fadeIn {
+  0% {
+    opacity: 0;
+  }
+  100% {
+    opacity: 1;
+  }
+}
+
+@keyframes fadeIn {
+  0% {
+    opacity: 0;
+  }
+  100% {
+    opacity: 1;
+  }
+}
+
+.animatedOut {
+  -webkit-animation-duration: 2s;
+  animation-duration: 2s;
+  -webkit-animation-fill-mode: both;
+  animation-fill-mode: both;
+  -webkit-animation-name: fadeOut;
+  animation-name: fadeOut;
+  border: none !important;
+}
+
+@-webkit-keyframes fadeOut {
+  0% {
+    opacity: 1;
+  }
+  100% {
+    opacity: 0;
+  }
+}
+
+@keyframes fadeOut {
+  0% {
+    opacity: 1;
+  }
+  100% {
+    opacity: 0;
+  }
+}
+
+.btnfixed {
+  position: fixed;
+  width: 3rem;
+  height: 3rem;
+  bottom: 20px;
+  right: 30px;
+  z-index: 10;
+  border: none ;
+  outline: none;
+  background-color: rgb(0, 0, 0, 0.8) ;
+  color: #FFF;
+  padding: 15px;
+  border-radius: 50%;
+  font-size: 18px;
+  transition: background-color 20s ease;
+}
+
+</style>
